c3d6e52803c49676b48d294d03c4e3e8af09b338
christianmartinroffey <c.martinroffey@gmail.com>
2022-05-20 15:35:30 +0000

    added code to generate the suits and numbers randomly

--- src/app.js
+++ src/app.js
@@ -4,56 +4,70 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-//suits in text
-const spades = "&spades";
-const clubs = "&clubs";
-const hearts = "&hearts";
-const diamonds = "&diams";
 
-const deckNumbers = [
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K"
-];
+//suits in text +
+const spades = "&#9824;";
+const clubs = "&#9827;";
+let hearts = "&#9829";
+let diamonds = "&#9830";
 const deckSuit = [0, 1, 2, 3];
+const deckNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 // onload needs to load a new card number and suit
 //add in timer of 10s
 window.onload = function() {
-  //write your code here
-  console.log();
+  numberID.innerHTML = cardNumber;
+  suitIDTop.innerHTML = suitNumber;
+  suitIDBottom.innerHTML = suitNumber;
+  console.log(numberID);
+
+  //ifs for top suits
+  if (suitNumber === 0) {
+    suitIDTop.innerHTML = spades;
+  }
+  if (suitNumber === 1) {
+    suitIDTop.innerHTML = clubs;
+  }
+  if (suitNumber === 2) {
+    suitIDTop.innerHTML = hearts;
+  }
+  if (suitNumber === 3) {
+    suitIDTop.innerHTML = diamonds;
+  }
+
+  //ifs for top suits
+  if (suitNumber === 0) {
+    suitIDBottom.innerHTML = spades;
+  }
+  if (suitNumber === 1) {
+    suitIDBottom.innerHTML = clubs;
+  }
+  if (suitNumber === 2) {
+    suitIDBottom.innerHTML = hearts;
+  }
+  if (suitNumber === 3) {
+    suitIDBottom.innerHTML = diamonds;
+  }
 };
 
-const generateRandom = Math.floor(Math.random() * deckNumbers.length);
+//Ids
+let suitIDTop = document.getElementById("top-suit");
+let numberID = document.getElementById("card-number");
+let suitIDBottom = document.getElementById("bottom-suit");
 
-const generateCard = () => {};
+//cardnumber generation
+let cardNumber = Math.floor(Math.random() * deckNumbers.length + 1);
+//suit generation
+let suitNumber = Math.floor(Math.random() * deckSuit.length);
+console.log(suitNumber);
 
-const generateSuit = () => {};
-// need to randomly generate a number 1-13
-//optional is to add a joker
-// random function for card number here
-//math.floor(math.random)*deckNumbers.length (will generate a number 0-12)
-
-//random function for suit here
-// needs to be 1-4
-//math.floor(math.random)*deckSuit.length (will generate a number 0-3)
+// console.log(cardNumber);
+// console.log(suitNumber);
 
 // here is where you can generate a new card on click
 //needs updating to replace 'alert' with the generate function above
 
 document.getElementById("new-card").addEventListener("click", function() {
-  alert("Give me a new card");
+  alert(String(suitNumber) + String(cardNumber));
 
   //add .value(two functions)
 });
-
-// joker - set it to sdisplay when the sum hit X number
