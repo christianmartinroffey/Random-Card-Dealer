a99996e789db014f0375e77a3bd0b99e27dd4b85
christianmartinroffey <c.martinroffey@gmail.com>
2022-07-19 13:19:00 +0000

    refactored code to fix card styling, color no being applied correctly and letters not generating correctly for the J,Q,K,A

--- src/app.js
+++ src/app.js
@@ -2,117 +2,62 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-//suits in text 
-const spades = "&#9824;";
-const clubs = "&#9827;";
-let hearts = "&#9829";
-// hearts.style.color = "red";
-let diamonds = "&#9830";
-// diamonds.style.color = "red";
-const deckSuit = [0, 1, 2, 3];
-const deckNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+const newCard = () => {
 
-let newCard = () => {
- 
-//Ids
-//note for future, can use the class but need add all of the classes to be able to use both in the same variable
-let suitIDTop = document.getElementById("top-suit");
-let numberID = document.getElementById("card-number");
-let suitIDBottom = document.getElementById("bottom-suit");
+const deckSuits = ["Hearts", "Diamonds", "Spades", "Clubs"];
+const deckNumbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-//cardnumber generation
-let cardNumber = Math.floor(Math.random() * deckNumbers.length + 1); //+1 ensures that it starts at 1
-//suit generation
-let suitNumber = Math.floor(Math.random() * deckSuit.length);
+const cards = [];
+for (let s = 0; s < deckSuits.length; s++) {
+  for (let v = 0; v < deckNumbers.length; v++) {
+    const value = deckNumbers[v];
+    const suit = deckSuits[s];
+    cards.push({ value, suit });
+  }
+}
 
-numberID.innerHTML = cardNumber;
-suitIDTop.innerHTML = suitNumber;
-suitIDBottom.innerHTML = suitNumber;
-
-  //ifs for top suits
-  let SuitFunction = () => {
-  if (suitNumber === 0) {
-    suitIDTop.innerHTML = spades;
-  }
-  if (suitNumber === 1) {
-    suitIDTop.innerHTML = clubs;
-  }
-  if (suitNumber === 2) {
-    suitIDTop.innerHTML = hearts;
-  }
-  if (suitNumber === 3) {
-    suitIDTop.innerHTML = diamonds;
-  }
-   //ifs for bottom suits
-   if (suitNumber === 0) {
-    suitIDBottom.innerHTML = spades;
-  }
-  if (suitNumber === 1) {
-    suitIDBottom.innerHTML = clubs;
-  }
-  if (suitNumber === 2) {
-    suitIDBottom.innerHTML = hearts;
-  }
-  if (suitNumber === 3) {
-    suitIDBottom.innerHTML = diamonds;
-  }
+return cards;
 };
 
-SuitFunction();
-//ifs for suit colour
-  if(suitNumber === 2 ){
-    document.getElementById("top-suit").style.color = "red";
-  }
-  if(suitNumber === 3 ){
-    document.getElementById("top-suit").style.color = "red";
-  }
-  if(suitNumber === 2 ){
-    document.getElementById("bottom-suit").style.color = "red";
-  }
-  if(suitNumber === 3 ){
-    document.getElementById("bottom-suit").style.color = "red";
-  }
+console.log(newCard());
 
-//ifs to change 1,11,12,13 to A, J,Q,K
-//note - could have added these directly into the array and saved the ifs
-let numbersToSigns = () => {
-  if(cardNumber === 1){
-    cardNumber = "A";
-  }
 
-  if(cardNumber === 11){
-  cardNumber = "J";
-}
+const randomCard = () => {
 
-if(cardNumber === 12){
-  cardNumber = "Q";
-}
+  const random = Math.floor(Math.random() * 51);  
+  const cardValue = cards[random].value;
+  const cardSuit = cards[random].suit;
 
-if(cardNumber === 13){
-  cardNumber = "K";
-}
+  let entity;
+  cardSuit === "Diamonds" ? (entity = "&diams;") : (entity = "&" + cardSuit.toLowerCase() + ";");
 
+  const card = document.createElement("div");
+    card.classList.add("card", cardSuit.toLowerCase());
+    card.innerHTML = 
+    '<span class="card-value-suit top">' + cardValue + entity + '</span>' + 
+    '<span class="card-suit">' + cardValue + '</span>' + 
+    '<span class="card-value-suit bot">' + cardValue + entity + '</span>';
+    document.body.appendChild(card);  
 
 };
 
-numbersToSigns();
-};
+const cards = newCard();
+randomCard(cards);
+
+
 
 // onload needs to load a new card number and suit
 //add in timer of 10s
 // note: have to add the function for new card and then call that function in the onload and button eventlistner
-window.onload = function() {
-  newCard();
+// window.onload = function() {
+  
 
-//reload automatically after 10s
-// setTimeout(function() {
-//   location.reload();
-// }, 10000);
+// //reload automatically after 10s
+// // setTimeout(function() {
+// //   location.reload();
+// // }, 10000);
 
-document.getElementById("new-card").addEventListener("click", (newCard));
-};
+// };
 
 
